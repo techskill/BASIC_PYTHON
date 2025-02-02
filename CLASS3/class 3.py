3f004339ea02818ac95e237f9e7d7a8834307466
Tech Skill <155097713+techskill@users.noreply.github.com>
2025-02-02 19:46:55 +0500

    Add files via upload

---
+++ CLASS3/class 3.py
@@ -0,0 +1,15 @@
+print('hello world') # print is function to print any statemnt in screen
+name='taha' # '' , "" any thing write in single and double quotation is string
+age=18     # integer
+attendance=True # boolean
+weight=40.5  # float like 1.5 and 20.5 is float
+
+
+print(type(name)) # type is function to find variable type
+print(type(age))
+print(type(attendance))
+print(type(weight))
+
+print(id(name)) # id is a memory loacation like home address
+
+
